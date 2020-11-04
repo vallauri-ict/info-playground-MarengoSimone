e49411516356b6ba159977f7fe27614ea64bb28a
MarengoSimone <62547054+MarengoSimone@users.noreply.github.com>
2020-11-04 17:20:25 +0100

    Ricaricato esercizio 1: Multiform

---
+++ 01-Multiform/MultiForm01/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace MultiForm01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnForm2 = new System.Windows.Forms.Button();
+            this.btnApriForm2b = new System.Windows.Forms.Button();
+            this.txtPassata = new System.Windows.Forms.TextBox();
+            this.btnPassaTxt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnForm2
+            // 
+            this.btnForm2.Location = new System.Drawing.Point(24, 24);
+            this.btnForm2.Name = "btnForm2";
+            this.btnForm2.Size = new System.Drawing.Size(103, 29);
+            this.btnForm2.TabIndex = 0;
+            this.btnForm2.Text = "Apri Form2";
+            this.btnForm2.UseVisualStyleBackColor = true;
+            this.btnForm2.Click += new System.EventHandler(this.btnForm2_Click);
+            // 
+            // btnApriForm2b
+            // 
+            this.btnApriForm2b.Location = new System.Drawing.Point(195, 24);
+            this.btnApriForm2b.Name = "btnApriForm2b";
+            this.btnApriForm2b.Size = new System.Drawing.Size(103, 29);
+            this.btnApriForm2b.TabIndex = 1;
+            this.btnApriForm2b.Text = "Apri Form2b";
+            this.btnApriForm2b.UseVisualStyleBackColor = true;
+            this.btnApriForm2b.Click += new System.EventHandler(this.btnApriForm2b_Click);
+            // 
+            // txtPassata
+            // 
+            this.txtPassata.Location = new System.Drawing.Point(24, 96);
+            this.txtPassata.Name = "txtPassata";
+            this.txtPassata.Size = new System.Drawing.Size(136, 20);
+            this.txtPassata.TabIndex = 2;
+            // 
+            // btnPassaTxt
+            // 
+            this.btnPassaTxt.Location = new System.Drawing.Point(195, 94);
+            this.btnPassaTxt.Name = "btnPassaTxt";
+            this.btnPassaTxt.Size = new System.Drawing.Size(103, 23);
+            this.btnPassaTxt.TabIndex = 3;
+            this.btnPassaTxt.Text = "Passa TXT a F2";
+            this.btnPassaTxt.UseVisualStyleBackColor = true;
+            this.btnPassaTxt.Click += new System.EventHandler(this.btnPassaTxt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPassaTxt);
+            this.Controls.Add(this.txtPassata);
+            this.Controls.Add(this.btnApriForm2b);
+            this.Controls.Add(this.btnForm2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnForm2;
+        private System.Windows.Forms.Button btnApriForm2b;
+        private System.Windows.Forms.TextBox txtPassata;
+        private System.Windows.Forms.Button btnPassaTxt;
+    }
+}
+
---
+++ 01-Multiform/MultiForm01/Form2.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MultiForm01
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLeggiValore = new System.Windows.Forms.Button();
+            this.txtForm2 = new System.Windows.Forms.TextBox();
+            this.btnInvia = new System.Windows.Forms.Button();
+            this.btnFormFiglia = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLeggiValore
+            // 
+            this.btnLeggiValore.Location = new System.Drawing.Point(12, 12);
+            this.btnLeggiValore.Name = "btnLeggiValore";
+            this.btnLeggiValore.Size = new System.Drawing.Size(119, 54);
+            this.btnLeggiValore.TabIndex = 0;
+            this.btnLeggiValore.Text = "Leggi Valore";
+            this.btnLeggiValore.UseVisualStyleBackColor = true;
+            this.btnLeggiValore.Click += new System.EventHandler(this.btnLeggiValore_Click);
+            // 
+            // txtForm2
+            // 
+            this.txtForm2.Location = new System.Drawing.Point(12, 99);
+            this.txtForm2.Name = "txtForm2";
+            this.txtForm2.Size = new System.Drawing.Size(132, 20);
+            this.txtForm2.TabIndex = 1;
+            // 
+            // btnInvia
+            // 
+            this.btnInvia.Location = new System.Drawing.Point(202, 96);
+            this.btnInvia.Name = "btnInvia";
+            this.btnInvia.Size = new System.Drawing.Size(75, 23);
+            this.btnInvia.TabIndex = 2;
+            this.btnInvia.Text = "Invia a Form";
+            this.btnInvia.UseVisualStyleBackColor = true;
+            this.btnInvia.Click += new System.EventHandler(this.btnInvia_Click);
+            // 
+            // btnFormFiglia
+            // 
+            this.btnFormFiglia.Location = new System.Drawing.Point(12, 152);
+            this.btnFormFiglia.Name = "btnFormFiglia";
+            this.btnFormFiglia.Size = new System.Drawing.Size(119, 33);
+            this.btnFormFiglia.TabIndex = 3;
+            this.btnFormFiglia.Text = "Apri Form Figlia";
+            this.btnFormFiglia.UseVisualStyleBackColor = true;
+            this.btnFormFiglia.Click += new System.EventHandler(this.btnFormFiglia_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 344);
+            this.Controls.Add(this.btnFormFiglia);
+            this.Controls.Add(this.btnInvia);
+            this.Controls.Add(this.txtForm2);
+            this.Controls.Add(this.btnLeggiValore);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLeggiValore;
+        private System.Windows.Forms.TextBox txtForm2;
+        private System.Windows.Forms.Button btnInvia;
+        private System.Windows.Forms.Button btnFormFiglia;
+    }
+}
