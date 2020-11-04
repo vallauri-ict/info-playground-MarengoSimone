5e0af5806e9bc3da8b3b84fb3485969dc96a261d
MarengoSimone <62547054+MarengoSimone@users.noreply.github.com>
2020-11-04 17:30:45 +0100

    Aggiunto esercizio 9: Classe Indexata

---
+++ 09-ClasseIndexata/09-ClasseIndexata/MyIndexClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_ClasseIndexata
+{
+    class MyIndexClass
+    {
+        private string[] element;
+
+        public MyIndexClass(int elementNumber)   // costruttore
+        {
+            element = new string[elementNumber];
+            for (int i = 0; i < element.Length; i++)
+            {
+                element[i] = "empty";
+            }
+        }
+
+        // Iteratore
+        public string this[int pos]    // viene richiamato quando faccio per esempio: vet[1] = "pippo"
+        {
+            get { return element[pos]; }
+            set { element[pos] = value; }
+        }
+
+        public int length()  // imposto la lunghezza del vettore così posso vederla e usarla nel Form 1
+        {
+            return element.Length;
+        }
+    }
+}
---
+++ 09-ClasseIndexata/README.md
@@ -0,0 +1,5 @@
+# info-playground-MarengoSimone
+
+_Stud. Marengo Simone_
+
+### Progetto 9: Primo esercizio con utilizzo della classe Indexata
