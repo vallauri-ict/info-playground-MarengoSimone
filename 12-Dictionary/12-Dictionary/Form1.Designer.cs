675bdb0d9e60188774b6659cb8c9e4e4424b3239
MarengoSimone <62547054+MarengoSimone@users.noreply.github.com>
2020-11-13 17:20:04 +0100

    Aggiunto esercizio 12: Dictionary

---
+++ 12-Dictionary/12-Dictionary/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace _12_Dictionary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAutore = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitolo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnVisualizza = new System.Windows.Forms.Button();
+            this.btnDictionary = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAutore
+            // 
+            this.txtAutore.Location = new System.Drawing.Point(142, 81);
+            this.txtAutore.Name = "txtAutore";
+            this.txtAutore.Size = new System.Drawing.Size(100, 20);
+            this.txtAutore.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Autore:";
+            // 
+            // txtTitolo
+            // 
+            this.txtTitolo.Location = new System.Drawing.Point(142, 34);
+            this.txtTitolo.Name = "txtTitolo";
+            this.txtTitolo.Size = new System.Drawing.Size(100, 20);
+            this.txtTitolo.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Titolo:";
+            // 
+            // btnVisualizza
+            // 
+            this.btnVisualizza.Location = new System.Drawing.Point(180, 144);
+            this.btnVisualizza.Name = "btnVisualizza";
+            this.btnVisualizza.Size = new System.Drawing.Size(120, 29);
+            this.btnVisualizza.TabIndex = 11;
+            this.btnVisualizza.Text = "Visualizza";
+            this.btnVisualizza.UseVisualStyleBackColor = true;
+            this.btnVisualizza.Click += new System.EventHandler(this.btnVisualizza_Click);
+            // 
+            // btnDictionary
+            // 
+            this.btnDictionary.Location = new System.Drawing.Point(26, 144);
+            this.btnDictionary.Name = "btnDictionary";
+            this.btnDictionary.Size = new System.Drawing.Size(120, 29);
+            this.btnDictionary.TabIndex = 10;
+            this.btnDictionary.Text = "Aggiungi";
+            this.btnDictionary.UseVisualStyleBackColor = true;
+            this.btnDictionary.Click += new System.EventHandler(this.btnDictionary_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 243);
+            this.Controls.Add(this.btnVisualizza);
+            this.Controls.Add(this.btnDictionary);
+            this.Controls.Add(this.txtAutore);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTitolo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Dictionary";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAutore;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTitolo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnVisualizza;
+        private System.Windows.Forms.Button btnDictionary;
+    }
+}
+
---
+++ 12-Dictionary/README.md
@@ -0,0 +1,7 @@
+# info-playground-MarengoSimone
+
+_Stud. Marengo Simone_
+
+### Progetto 12: Esercizio dove è stato utilizzato per la prima volta dictionary. L'esercizio è una sorta di proseguo dell'esercizio 11
+in quanto mantiene la stessa struttura a livello di progettazione grafica. Per una questione di leggibilità e comprensione ho preferito 
+dividere i 2 esercizi.
