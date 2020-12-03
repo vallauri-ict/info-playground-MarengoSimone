648b535109ec98f2a4310be519f581afff7a0ace
MarengoSimone <62547054+MarengoSimone@users.noreply.github.com>
2020-12-03 17:10:49 +0100

    Aggiunto esercizio 18: Ereditarietà 3

---
+++ 18-EreditarietàEsGuidato03/18-EreditarietàEsGuidato03/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Le informazioni generali relative a un assembly sono controllate dal seguente 
+// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+// associate a un assembly.
+[assembly: AssemblyTitle("18-EreditarietàEsGuidato03")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("18-EreditarietàEsGuidato03")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili
+// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da
+// COM, impostare su true l'attributo ComVisible per tale tipo.
+[assembly: ComVisible(false)]
+
+// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
+[assembly: Guid("6026fbeb-2d2f-4f25-8c8f-0dda0f51f502")]
+
+// Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+//
+//      Versione principale
+//      Versione secondaria
+//      Numero di build
+//      Revisione
+//
+// È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
+// usando l'asterisco '*' come illustrato di seguito:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 18-EreditarietàEsGuidato03/README.md
@@ -0,0 +1,5 @@
+# info-playground-MarengoSimone
+
+_Stud. Marengo Simone_
+
+### Progetto 18: Esercizio sull'ereditarietà di prova per verificare le differenze tra il passaggio degli oggetti per valore, tramite ref e tramite out.
