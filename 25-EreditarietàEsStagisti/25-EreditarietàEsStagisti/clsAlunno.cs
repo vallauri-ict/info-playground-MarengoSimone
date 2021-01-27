b9498b7dfa6fcae66589764dc6a5a989b88f214a
MarengoSimone <62547054+MarengoSimone@users.noreply.github.com>
2021-01-27 09:58:32 +0100

    Aggiunto esercizio 25: Esercizio Stagisti

---
+++ 25-EreditarietàEsStagisti/25-EreditarietàEsStagisti/clsAlunno.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_EreditarietàEsStagisti
+{
+    abstract class clsAlunno : clsPersona
+    {
+        #region Attributi
+        protected static int progressivo = 0;
+        protected readonly string matricola;
+        private char classe;
+        private char sezione;
+        private string specializzazione;
+        #endregion
+
+        public char Classe
+        {
+            get => classe;
+            set => classe = Convert.ToChar(value);
+        }
+        public string Matricola => matricola;
+        public char Sezione
+        {
+            get => sezione;
+            set => sezione = Convert.ToChar(value);
+        }
+        public string Specializzazione
+        {
+            get => specializzazione;
+            set => specializzazione = value;
+        }
+        protected clsAlunno(string nome,string cognome,string città) : base(nome,cognome,città)
+        {
+        }
+        public override string visualizza()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
