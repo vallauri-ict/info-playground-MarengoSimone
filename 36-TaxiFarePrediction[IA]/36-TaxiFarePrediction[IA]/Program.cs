dadf7748275dac8e45cc0f8806a77925057ff32a
MarengoSimone <62547054+MarengoSimone@users.noreply.github.com>
2021-05-26 17:36:43 +0200

    Primo caricamento esercizio 36: Taxi Fare Prediction

---
+++ 36-TaxiFarePrediction[IA]/36-TaxiFarePrediction[IA]/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _36_TaxiFarePrediction_IA_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
